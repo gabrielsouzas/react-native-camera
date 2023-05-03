6fc682cb97075c1245895630747a8bbf6bb987aa
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-05-03 15:39:04 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Modal, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 import { useEffect, useRef, useState } from 'react';
 import { FontAwesome } from '@expo/vector-icons';
@@ -9,11 +9,13 @@ export default function App() {
   const [type, setType] = useState(Camera.Constants.Type.back)
   const [hasPermission, setHasPermission] = useState(null)
   const [capturedPhoto, setCapturedPhoto] = useState(null)
+  const [open, setOpen] = useState(false)
 
   async function takePicture() {
     if (camRef) {
       const data = await camRef.current.takePictureAsync();
       setCapturedPhoto(data.uri)
+      setOpen(true)
     }
   }
 
@@ -37,6 +39,7 @@ export default function App() {
       <Camera
         style={styles.camera}
         type={type}
+        ref={camRef}
       >
         <View style={styles.contentButtons}>
           <TouchableOpacity
@@ -59,6 +62,24 @@ export default function App() {
           </TouchableOpacity>
         </View>
       </Camera>
+      {capturedPhoto &&(
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={open}
+      >
+        <View style={styles.contentModal}>
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => { setOpen(false) }}
+          >
+            <FontAwesome name='close' size={50} color='#fff'></FontAwesome>
+          </TouchableOpacity>
+          
+          <Image style={styles.imgPhoto} source={{ uri:capturedPhoto}}/>
+        </View>
+      </Modal>
+      )}
     </SafeAreaView>
   );
 }
@@ -100,5 +121,21 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     borderRadius: 50,
-  }
+  },
+  contentModal: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    margin:20,
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    left: 2,
+    margin: 10,
+  },
+  imgPhoto: {
+    width: '100%',
+    height: 400,
+  },
 });
